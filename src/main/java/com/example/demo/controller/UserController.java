fc7e2c22ed1f92241d7f4c3a8d6992180eae0f5f
Hosein118 <mr.hoseinrahimi@gmail.com>
2021-03-13 00:36:17 +0330

    fix: ResponseEntity for delete

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -8,6 +8,7 @@ import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -47,8 +48,8 @@ public class UserController {
     }
 
     @DeleteMapping("/{id}")
-    public void deleteUserById(@RequestParam Integer id) {
-        this.userService.deleteUserById(id);
+    public ResponseEntity<Void> deleteUserById(@RequestParam Integer id) {
+        return this.userService.deleteUserById(id);
     }
 
 }
