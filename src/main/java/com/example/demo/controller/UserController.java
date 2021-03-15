44abc9f5dd306a849b815feb4bbb8f440f835633
Hosein118 <mr.hoseinrahimi@gmail.com>
2021-03-15 10:01:58 +0330

    refact: remove post api from usercontroller

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,11 +1,8 @@
 package com.example.demo.controller;
 
 import java.util.List;
-import java.util.Optional;
 
-import com.example.demo.client.PostClient;
 import com.example.demo.client.UserClient;
-import com.example.demo.entity.Post;
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 
@@ -33,19 +30,9 @@ public class UserController {
         return this.userService.userClient();
     }
 
-    @GetMapping("/post/client")
-    public List<PostClient> postClient() {
-        return this.userService.postClient();
-    }
-
     @GetMapping("/user/update")
     public void updateUserclient() {
         this.userService.updateUserclient();
-    }
-
-    @GetMapping("/post/update")
-    public void updatePostClient() {
-        this.userService.updatePostClient();
     }
 
     @GetMapping("/user/all")
@@ -53,30 +40,14 @@ public class UserController {
         return this.userService.getalluser();
     }
 
-    @GetMapping("/post/all")
-    public List<Post> getallPosts() {
-        return this.userService.getallPost();
-    }
-
     @GetMapping("/user/{id}")
-    public ResponseEntity<Optional<User>> getUserById(@PathVariable Integer id) {
+    public ResponseEntity<User> getUserById(@PathVariable Integer id) {
         return this.userService.findUserById(id);
-    }
-
-    @GetMapping("/post/{id}")
-    public ResponseEntity<Optional<Post>> getPostById(@PathVariable Integer id) {
-        return this.userService.findPostById(id);
     }
 
     @DeleteMapping("/user/{id}")
     public ResponseEntity<String> deleteUserById(@PathVariable Integer id) {
         return this.userService.deleteUserById(id);
-    }
-
-    @DeleteMapping("/post/{id}")
-    public ResponseEntity<String> deletePostById(@PathVariable Integer id) {
-        return this.userService.deletePostById(id);
-
     }
 
 }
