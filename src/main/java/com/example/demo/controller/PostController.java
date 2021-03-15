feed3024b0121109404e6d068936e0948e6ffce7
Hosein118 <mr.hoseinrahimi@gmail.com>
2021-03-15 09:59:01 +0330

    refact: create postcontroller

---
+++ src/main/java/com/example/demo/controller/PostController.java
@@ -0,0 +1,48 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import com.example.demo.client.PostClient;
+import com.example.demo.entity.Post;
+import com.example.demo.service.PostService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+public class PostController {
+
+    private PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping("/post/client")
+    public List<PostClient> postClient() {
+        return this.postService.postClient();
+    }
+
+    @GetMapping("/post/update")
+    public void updatePostClient() {
+        this.postService.updatePostClient();
+    }
+
+    @GetMapping("/post/all")
+    public List<Post> getallPosts() {
+        return this.postService.getallPost();
+    }
+
+    @GetMapping("/post/{id}")
+    public ResponseEntity<Post> getPostById(@PathVariable Integer id) {
+        return this.postService.findPostById(id);
+    }
+
+    @DeleteMapping("/post/{id}")
+    public ResponseEntity<String> deletePostById(@PathVariable Integer id) {
+        return this.postService.deletePostById(id);
+
+    }
+
+}
