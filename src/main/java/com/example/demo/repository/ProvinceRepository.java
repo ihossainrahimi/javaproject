1d9ec94f95bc918cf0c2fc5b1d47fa29b715a72f
Hosein118 <mr.hoseinrahimi@gmail.com>
2021-04-12 12:49:38 +0430

    feat:create repository for province

---
+++ src/main/java/com/example/demo/repository/ProvinceRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Province;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProvinceRepository extends JpaRepository<Province, Integer> {
+    
+}
